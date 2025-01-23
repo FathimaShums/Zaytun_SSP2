9c7c936516b59156b3a9896586dfb94d4ac075b0
FathimaShums <fathimashums416@gmail.com>
2025-01-23 11:31:33 +0530

    role based content

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('HomePage') }}
         </h2>
     </x-slot>
 
@@ -10,13 +10,25 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 <!-- Display the message if the user is an admin -->
                 @if(auth()->user() && auth()->user()->RoleID == 1) <!-- Assuming RoleID 1 is for Admin -->
-                    <div class="text-center text-xl font-semibold text-green-500">
-                        Hello, you've logged in as the admin.
-                        
-        <livewire:manage-employees />
+                    <div class="text-center text-xl font-semibold ">                
+                     <livewire:manage-employees />
 
                     </div>
                 @endif
+                @if(auth()->user() && auth()->user()->RoleID == 2) <!-- Assuming RoleID 1 is for Admin -->
+                <div>
+                    <h2>"employees's account</h2>
+                </div>
+                @if(auth()->user() && auth()->user()->RoleID == 3) <!-- Assuming RoleID 1 is for Admin -->
+                <div>
+                    <h2>"employee-delivery's account</h2>
+                </div>
+                @if(auth()->user() && auth()->user()->RoleID == 4) <!-- Assuming RoleID 1 is for Admin -->
+                <div>
+                    <h2>"customer's account</h2>
+                </div>
+
+            @endif
 
                 <!-- Include the Welcome component -->
                 <x-welcome />
