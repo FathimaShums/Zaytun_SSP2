64a9d5492de062acc26bc5f84b2ff57161e64929
FathimaShums <fathimashums416@gmail.com>
2025-01-29 20:04:07 +0530

    endpoints to access food items

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\FoodItemController;
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,3 +13,11 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('food-items')->group(function () {
+    Route::get('/', [FoodItemController::class, 'index']); // List all food items
+    Route::post('/', [FoodItemController::class, 'store']); // Create a food item
+    Route::get('/{id}', [FoodItemController::class, 'show']); // Get a specific food item
+    Route::put('/{id}', [FoodItemController::class, 'update']); // Update a food item
+    Route::delete('/{id}', [FoodItemController::class, 'destroy']); // Delete a food item
+});
